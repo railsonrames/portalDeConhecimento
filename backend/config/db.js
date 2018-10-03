252df7f3e3b0f4b37a024b9641cea870f7207c1a
Railson Rames <rames.cloud@outlook.com.br>
2018-10-03 17:46:39 -0300

    Adicionando o arquivo de valiação de dados para o app e adicionei o comando para o knex realizar as migrations pelo consign

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 const config = require('../knexfile.js');
 const knex = require('knex')(config);
 
-// knex.migrate.latest([config]);
+knex.migrate.latest([config]); // temporáriamente ativo, para fins de desenvolvimento - o mais aconselhável é atuar manualmente através do comando "knex migrate:latest" via bash
 module.exports = knex;
