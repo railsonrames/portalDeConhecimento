5df87e15903e95daa938237639ceed7ab71643a3
Railson Rames <rames.cloud@outlook.com.br>
2018-10-10 14:20:55 -0300

    Fix nos artigos e adição da query para indexar os artigos por categoria

--- backend/api/article.js
+++ backend/api/article.js
@@ -1,8 +1,10 @@
+const queries = require('./queries')
+
 module.exports = app => {
     const { existsOrError, notExistsOrError } = app.api.validation
 
     const save = (req, res) => {
-        const article = { ... res.body }
+        const article = { ... req.body }
         if (req.params.id) article.id = req.params.id
 
         try {
@@ -67,5 +69,21 @@ module.exports = app => {
             .catch(err => res.status(500).send(err))
     }
 
-    return {save,remove,get,getById}
+    const getByCategory = async (req, res) => {
+        const categoryId = req.params.id
+        const page = req.query.page || 1
+        const categories = await app.db.raw(queries.categoryWithChildren, categoryId)
+        const ids = categories.rows.map(c => c.id)
+
+        app.db({a: 'articles', u: 'users'})
+            .select('a.id', 'a.name', 'a.description', 'a.imageUrl', { author: 'u.name' })
+            .limit(limit).offset(page * limit - limit)
+            .whereRaw('?? = ??', ['u.id', 'a.userId'])
+            .whereIn('categoryId', ids)
+            .orderBy('a.id','desc')
+            .then(articles => res.json(articles))
+            .catch(err => res.status(500).send(err))
+    }
+
+    return { save, remove, get, getById, getByCategory }
 }
