e599c8594bd9048a0a02c52daf7d0c93af89d8c2
Railson Rames <rames.cloud@outlook.com.br>
2018-10-11 18:10:41 -0300

    Um monte de ajustes, adicioando responsabilidades para o arquivo .env, api de status com o MongoDB, um schedule para o monitoramento e persistência das estatísiticas, uma migration para soft delete de usuário e os respectivos ajustes para a funcionalidade, validação de criação de usuário pela rota signin e pelo users para a permissão de admin

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -14,14 +14,14 @@ module.exports = app => {
         .all(app.config.passport.authenticate())
         .put(admin(app.api.user.save))
         .get(admin(app.api.user.getById))
-        // .delete(app.api.user.remove)
+        .delete(admin(app.api.user.remove))
         
     app.route('/categories')
         .all(app.config.passport.authenticate())
         .get(admin(app.api.category.get))
         .post(admin(app.api.category.save))
 
-    // Cuidado com a orde! Tem que vir antes de '/categories/:id'
+    // Cuidado com a ordem! Tem que vir antes de '/categories/:id'
     app.route('/categories/tree')
         .all(app.config.passport.authenticate())
         .get(app.api.category.getTree)
@@ -46,4 +46,8 @@ module.exports = app => {
     app.route('/categories/:id/articles')
         .all(app.config.passport.authenticate())
         .get(app.api.article.getByCategory)
+
+    app.route('/stats')
+        .all(app.config.passport.authenticate())
+        .get(app.api.stat.get)
 }
